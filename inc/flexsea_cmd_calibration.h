69ad43e8ed14fd9edf980cd9cfa387f607b7f6b1
David Weisdorf <weisdorf.david@gmail.com>
2017-02-08 10:16:07 -0500

    Added comments and file headers.

--- inc/flexsea_cmd_calibration.h
+++ inc/flexsea_cmd_calibration.h
@@ -21,10 +21,10 @@
 	Biomechatronics research group <http://biomech.media.mit.edu/>
 	[Contributors]
 *****************************************************************************
-	[This file] flexsea_cmd_control: commands specific to the motor & control
+	[This file] flexsea_cmd_calibration: commands specific to the calibration tools
 *****************************************************************************
 	[Change log] (Convention: YYYY-MM-DD | author | comment)
-	* 2016-09-09 | jfduval | Initial GPL-3.0 release
+	* 2017-02-07 | dweisdorf | Initial GPL-3.0 release
 	*
 ****************************************************************************/
 
@@ -44,16 +44,41 @@ extern "C" {
 // Prototype(s):
 //****************************************************************************
 
+/* Initializes part of the array of function pointers which determines which 
+	function to call upon receiving a message
+*/
 void init_flexsea_payload_ptr_calibration(void);
 
 //Calibration mode:
+
+
+/* Called by master to send a message to the slave, attempting to inititiate a calibration procedure specified by 
+	'calibrationMode'. Slave will respond with the calibration procedure it is running or about to run.
+*/
 void tx_cmd_calibration_mode_r(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, \
 						uint16_t *len, uint8_t calibrationMode);
+
+/* Called by master to send a message to the slave, attempting to inititiate a
+	calibration procedure specified by 'calibrationMode'. Slave will not respond.
+	TODO: rename this to 'write'
+*/
 void tx_cmd_calibration_mode_rw(uint8_t *shBuf, uint8_t *cmd, uint8_t *cmdType, \
 						uint16_t *len, uint8_t calibrationMode);
 
+/* Master calls this function automatically after receiving a response from slave
+*/
 void rx_cmd_calibration_mode_rr(uint8_t *buf, uint8_t *info);
+
+/* Slave calls this function automatically after receiving a read from master.
+	It determines what to do with the information passed to it, 	
+	And it replies indicating the resulting decision
+*/
 void rx_cmd_calibration_mode_rw(uint8_t *buf, uint8_t *info);
+
+/* Slave calls this function automatically after receiving a read from master.
+	It determines what to do with the information passed to it, 	
+	And it does not reply.
+*/
 void rx_cmd_calibration_mode_w(uint8_t *buf, uint8_t *info);
 
 #ifdef __cplusplus
