8a2f944f0d1871ca850616207fed6d5969fbf925
David Weisdorf <weisdorf.david@gmail.com>
2017-02-24 14:56:45 -0500

    adding C extern to make these fm_block_allocator work in c++

--- inc/fm_block_allocator.h
+++ inc/fm_block_allocator.h
@@ -31,6 +31,10 @@
 #ifndef FM_BLOCK_ALLOCATOR_H
 #define FM_BLOCK_ALLOCATOR_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 typedef unsigned int size_t;
 
 #define FM_BLOCK_SIZE 256
@@ -135,5 +139,9 @@ typedef struct {
 	char data[];
 } BlockWrapper;
 
+#ifdef __cplusplus
+}
 #endif
+
+#endif //FM_BLOCK_ALLOCATOR_H
 
