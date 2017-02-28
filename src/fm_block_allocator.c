4d4bca6f0b773dfddaab032e39133fbfa2045837
David Weisdorf <weisdorf.david@gmail.com>
2017-02-28 10:15:17 -0500

    Resolved error/warning to do w/ c/c++/Qt environment
     - swapped out 'bool' for uint8_t in c code. Didn't cause error before on some machines
     - renamed offsetof to fm_offsetof since Qt environment (and Eclipse?) provide in-built offsetof functions

--- src/fm_block_allocator.c
+++ src/fm_block_allocator.c
@@ -57,7 +57,7 @@ void fail(void) {
 	printf("failed\n");
 }
 
-inline bool isInterrupt()
+uint8_t isInterrupt()
 {
 	//return (SCB->ICSR & SCB_ICSR_VECTACTIVE_Msk) != 0 ;	//ToDo remove or isolate with #ifdef
 	return 0;
@@ -67,13 +67,13 @@ typedef struct Block Block;
 
 Block memory_pool[FM_NUM_BLOCKS];
 
-#define offsetof(st, m) ((size_t)(&((st *)0)->m))
+#define fm_offsetof(st, m) ((size_t)(&((st *)0)->m))
 
 static Block* get_block(void * raw_block) {
 	uint32_t p =  ((uint32_t) raw_block);
 	if ((p & 0x3) != 0)
 		fail();
-	size_t offset = offsetof(Block, data);
+	size_t offset = fm_offsetof(Block, data);
 	Block* b = raw_block - offset;
 	return b;
 }
