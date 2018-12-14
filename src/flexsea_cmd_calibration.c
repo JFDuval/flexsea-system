44861678ae603d5f5b125b2edb9b4000ff1cfdbb
Jean-Francois Duval <jfduval@dephy.com>
2018-12-14 15:56:38 -0500

    Compile hack for older M7 project that doesn't support new programmable I2t yet.

--- src/flexsea_cmd_calibration.c
+++ src/flexsea_cmd_calibration.c
@@ -250,6 +250,7 @@ uint8_t handleCalibrationMessage(uint8_t *buf, uint8_t write)
 					v = REBUILD_UINT16(buf, &index);
 					saveUVLO(v);
 				}
+				#ifndef TMP_COMPILE_HACK
 				else if(isI2T())
 				{
 					i2tBattR.shift = buf[index++];
@@ -259,6 +260,7 @@ uint8_t handleCalibrationMessage(uint8_t *buf, uint8_t write)
 					i2tBattR.config = buf[index++];
 					saveI2t(i2tBattR);
 				}
+				#endif	//TMP_COMPILE_HACK
 				#endif
 			}
 			calibrationFlagToRunOrIsRunning = calibrationFlags;
