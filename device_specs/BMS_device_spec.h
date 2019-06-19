363d458d997f637fb1696482614b884c4afd3f96
Jean-Francois Duval <jfduval@dephy.com>
2019-06-19 17:58:44 -0400

    Commented-our invalid device spec list

--- device_specs/BMS_device_spec.h
+++ device_specs/BMS_device_spec.h
@@ -11,10 +11,11 @@
 
 	#define DEVICE_FIELD_COUNT 24
 
+/*That's not C, and that's not right version
 #define	PROTOTYPE_POS 0
 #define	ID_POS 1
 #define	STATE_TIME_POS 2
-#define	CELLS[0].MV_POS 3
+#define	CELLS_0_MV_POS 3
 #define	CELLS[0].MAH_POS 4
 #define	CELLS[1].MV_POS 5
 #define	CELLS[1].MAH_POS 6
@@ -35,5 +36,5 @@
 #define	CELLCHARGE_POS 21
 #define	CELLDISCHARGE_POS 22
 #define	STATUS_POS 23
-
+ */
 
